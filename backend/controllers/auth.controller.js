6b357983b3f5a95f57cb8bab109a926582a61602
daniloromano99 <72809334+daniloromano99@users.noreply.github.com>
2024-06-26 14:00:31 +0200

    secondo commit - 26/06/2024 14:00

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,47 @@
-export const signup = (req, res) => {
+import User from '../models/user.model.js';
+
+
+export const signup = async (req, res) => {
     try {
         const {fullName,username,password,confirmPassword,gender} = req.body;
-    } catch(error) {
 
+        if(password !== confirmPassword)
+            {
+                return res.status(400).json({error:"Password errata"})
+            }
+        
+        const user = await User.findOne({username}); //chiamata asincrona(await) che serve a fare il check per vedere se esiste già l'username indicato nel DB   
+        if(user) {
+            return res.status(400).json({error:"Username già esistente"})
+        }        
+
+        //IMMAGINI PROFILO DEFAULT usando l'api al seguente link:
+        const profileImage_uomo = `https://avatar.iran.liara.run/public/boy?username=${username}` //immagine profilo uomo
+        const profileImage_donna = `https://avatar.iran.liara.run/public/girl?username=${username}` //immagine profilo donna
+        
+        // SE NON ENTRO IN QUEGLI IF CREO QUINDI L'UTENTE
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profileImage: gender === "male" ? profileImage_uomo : profileImage_donna 
+        })
+
+    await newUser.save(); //salvataggio asincrono
+
+    //è andato a buon fine quindi resituisce status 201 e aggiunge i dati al db
+    res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        gender: newUser.gender,
+        profileImage: newUser.profileImage
+    })
+
+    } catch(error) {
+        console.log("Errore nella registrazione", error.message);
+        res.status(500).json({error: "Internal Server Error"});
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,18 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res) => {
+dotenv.config();
+app.use(express.json()); //per fare il parsing la richieste che arrivano con il payload JSON (da req.body nel controller)
+app.use("/api/auth", authRoutes);
+
+/*app.get("/", (req,res) => {
     //root route localhost:5000
     res.send("<h2> SCOSSA? VA BENE <\h2>");
 });
 
-app.use("/api/auth", authRoutes)
+*/
 
 app.listen(PORT,() => {
     connectToMongoDB();
